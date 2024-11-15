579b1d169f4003e7b6fc93c33849b4fddd024de1
arvinkhan <arveenkhan323@gmail.com>
2024-11-16 00:53:26 +0530

    few changes

--- SwiftRoute.py
+++ SwiftRoute.py
@@ -1,138 +1,87 @@
-import cv2
-import pandas as pd
-from ultralytics import YOLO
-from tracker import *
+from SwiftRoute import CarCounter
+import time
 
-def CarCounter(traffic_video):
-    model = YOLO('yolov9c.pt')
-    model.overrides['verbose'] = True
-    model.overrides['device'] = 'cuda' 
-    class_list = [
-        'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 
-        'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 
-        'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 
-        'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 
-        'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 
-        'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 
-        'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 
-        'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 
-        'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 
-        'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 
-        'toothbrush'
-    ]
-    tracker = Tracker()
-    count = 0
-    cap = cv2.VideoCapture(traffic_video)
+Traffic1 = CarCounter('Road_A.mp4')
+Traffic2 = CarCounter('Road_B.mp4')
+Traffic3 = CarCounter('Road_C.mp4')
+Traffic4 = CarCounter('Road_D.mp4')
 
-    counter_down_cars = set()
-    counter_down_motorcycles = set()
-    counter_up_cars = set()
-    counter_up_motorcycles = set()
+class Road:
+    def __init__(self, Name: str, Light_Color: str, Direction: str, Vehicle_Amount=0, People_Amount=0, Time_Interval=180):
+        self.Name = Name
+        self.Light_Color = Light_Color
+        self.Direction = Direction
+        self.Vehicle_Amount = Vehicle_Amount
+        self.People_Amount = People_Amount
+        self.Time_Interval = Time_Interval
     
-    left_people = set()
-    right_people = set()
+    def LightChange(self, Ain, Bin, Cin, Din, Aout, Bout):
+        if 3 * Bin.People_Amount + 3 * Bout.People_Amount > Bin.Vehicle_Amount + Bout.Vehicle_Amount:
+            Ain.Light_Color = "Green"
+            Bin.Light_Color = "Red"
+            Cin.Light_Color = "Red"
+            Din.Light_Color = "Green"
+        elif 3 * Bin.People_Amount + 3 * Bout.People_Amount > Ain.Vehicle_Amount + Aout.Vehicle_Amount:
+            Ain.Light_Color = "Green"
+            Bin.Light_Color = "Red"
+            Cin.Light_Color = "Red"
+            Din.Light_Color = "Green"
+        else:
+            Ain.Light_Color, Din.Light_Color, Aout.Light_Color, Dout.Light_Color, Bin.Light_Color, Cin.Light_Color, Bout.Light_Color, Cout.Light_Color = Bin.Light_Color, Cin.Light_Color, Bout.Light_Color, Cout.Light_Color, Ain.Light_Color, Din.Light_Color, Aout.Light_Color, Dout.Light_Color 
+            
+            pass
+        return Ain.Light_Color, Bin.Light_Color, Cin.Light_Color, Din.Light_Color
 
-    y_line = 308  
-    left_line = 0  # Vertical line position for extreme left detection
-    right_line = None  # This will be set to the frame width later
-
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        count += 1
-        results = model.predict(frame)
+    def TimeInterval(self, Ain, Bin, Cin, Din, C_Total):
+        Ain.Time_Interval = 180 * (Ain.Vehicle_Amount + Din.Vehicle_Amount) / C_Total
+        Bin.Time_Interval = (180 * (Cin.Vehicle_Amount + Bin.Vehicle_Amount) / C_Total) + (180 - Ain.Time_Interval)
+        Cin.Time_Interval = (180 * (Cin.Vehicle_Amount + Bin.Vehicle_Amount) / C_Total) + (180 - Ain.Time_Interval)
+        Din.Time_Interval = 180 * (Ain.Vehicle_Amount + Din.Vehicle_Amount) / C_Total
+        Aout.Time_Interval = 180 * (Ain.Vehicle_Amount + Din.Vehicle_Amount) / C_Total
+        Bout.Time_Interval = (180 * (Cin.Vehicle_Amount + Bin.Vehicle_Amount) / C_Total) + (180 - Ain.Time_Interval)
+        Cout.Time_Interval = (180 * (Cin.Vehicle_Amount + Bin.Vehicle_Amount) / C_Total) + (180 - Ain.Time_Interval)
+        Dout.Time_Interval = 180 * (Ain.Vehicle_Amount + Din.Vehicle_Amount) / C_Total
         
-        a = results[0].boxes.data
-        a = a.detach().cpu().numpy()  
-        px = pd.DataFrame(a).astype("float")
-        
-        detected_objects = []
-        
-        for index, row in px.iterrows():
-            x1, y1, x2, y2, d = int(row[0]), int(row[1]), int(row[2]), int(row[3]), int(row[5])
-            c = class_list[d]
+        return Ain.Time_Interval, Bin.Time_Interval, Cin.Time_Interval, Din.Time_Interval
 
-            if c in ['car', 'motorcycle', 'person', 'truck']:
-                detected_objects.append([x1, y1, x2, y2, c])
+# Create instances of Road
+Ain = Road("A", "Green", "East", Traffic1[0] + Traffic1[1], Traffic1[4])
+Bin = Road("B", "Red", "North", Traffic2[0] + Traffic2[1], Traffic2[4])
+Cin = Road("C", "Red", "West", Traffic3[0] + Traffic3[1], Traffic3[4])
+Din = Road("D", "Green", "South", Traffic4[0] + Traffic4[1], Traffic4[4])
+Aout = Road("A", "Green", "West", Traffic1[2] + Traffic1[3], Traffic1[5])
+Bout = Road("B", "Red", "South", Traffic2[2] + Traffic2[3], Traffic2[5])
+Cout = Road("C", "Red", "East", Traffic3[2] + Traffic3[3], Traffic3[5])
+Dout = Road("D", "Green", "North", Traffic4[2] + Traffic4[3], Traffic4[5])
 
-        bbox_id = tracker.update(detected_objects)
-
-        # Set the right line position to the width of the frame
-        frame_height, frame_width = frame.shape[:2]
-        right_line = frame_width  # Vertical line position for extreme right detection
-
-        for bbox in bbox_id:
-            x3, y3, x4, y4, id, obj_class = bbox
-            cx, cy = (x3 + x4) // 2, (y3 + y4) // 2
-            cv2.circle(frame, (cx, cy), 4, (0, 0, 255), -1)
-            cv2.rectangle(frame, (x3, y3), (x4, y4), (0, 255, 0), 2)
-            cv2.putText(frame, f'{obj_class} {id}', (cx, cy), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 255), 2)
-
-            # Counting cars and motorcycles moving downwards
-            if cx < frame.shape[1] // 2:  # Downward counting
-                if y_line < (cy + 7) and y_line > (cy - 7):
-                    if id not in counter_down_cars and id not in counter_down_motorcycles:  
-                        if obj_class == 'car':
-                            counter_down_cars.add(id)
-                        elif obj_class == 'motorcycle':
-                            counter_down_motorcycles.add(id)
-
-            # Counting cars and motorcycles moving upwards
-            if cx >= frame.shape[1] // 2:  # Upward counting
-                if y_line < (cy + 7) and y_line > (cy - 7):
-                    if id not in counter_up_cars and id not in counter_up_motorcycles:  
-                        if obj_class == 'car':
-                            counter_up_cars.add(id)
-                        elif obj_class == 'motorcycle':
-                            counter_up_motorcycles.add(id)
-
-            # Left line detection for people
-            if obj_class == 'person' and left_line < (cx + 7) and left_line > (cx - 7):
-                if id not in left_people:
-                    left_people.add(id)
-
-            # Right line detection for people
-            if obj_class == 'person' and right_line < (cx + 7) and right_line > (cx - 7):
-                if id not in right_people:
-                    right_people.add(id)
-
-        text_color = (255, 255, 255)
-        red_color = (0, 0, 255)   
-
-        cv2.line(frame, (0, y_line), (frame_width // 2, y_line), red_color, 3)
-        cv2.putText(frame, 'Down Line', (10, y_line - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, text_color, 1, cv2.LINE_AA)
-
-        cv2.line(frame, (frame_width // 2, y_line), (frame_width, y_line), red_color, 3)
-        cv2.putText(frame, 'Up Line', (frame_width // 2 + 10, y_line - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, text_color, 1, cv2.LINE_AA)
-
-        # Draw extreme left and right lines
-        cv2.line(frame, (left_line, 0), (left_line, frame_height), red_color, 3)
-        cv2.putText(frame, 'Left Line', (left_line + 10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, text_color, 1, cv2.LINE_AA)
-        cv2.line(frame, (right_line, 0), (right_line, frame_height), red_color, 3)
-        cv2.putText(frame, 'Right Line', (right_line + 10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, text_color, 1, cv2.LINE_AA)
-
-        cars_down = len(counter_down_cars)
-        motorcycles_down = len(counter_down_motorcycles)
-        cars_up = len(counter_up_cars)
-        motorcycles_up = len(counter_up_motorcycles)
-        people_left = len(left_people)
-        people_right = len(right_people)
-
-        cv2.putText(frame, f'Cars Down: {cars_down}', (60, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, red_color, 1, cv2.LINE_AA)
-        cv2.putText(frame, f'Motorcycles Down: {motorcycles_down}', (60, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, red_color, 1, cv2.LINE_AA)
-        cv2.putText(frame, f'Cars Up: {cars_up}', (300, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, red_color, 1, cv2.LINE_AA)
-        cv2.putText(frame, f'Motorcycles Up: {motorcycles_up}', (300, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, red_color, 1, cv2.LINE_AA)
-        cv2.putText(frame, f'People Left: {people_left}', (60, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, red_color, 1, cv2.LINE_AA)
-        cv2.putText(frame, f'People Right: {people_right}', (300, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, red_color, 1, cv2.LINE_AA)
-
-        cv2.imshow("frames", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-    return cars_down, motorcycles_down, cars_up, motorcycles_up, people_left, people_right
+Car_Total = Ain.Vehicle_Amount + Bin.Vehicle_Amount + Cin.Vehicle_Amount + Din.Vehicle_Amount
+P_Total = Ain.People_Amount + Bin.People_Amount + Cin.People_Amount + Din.People_Amount
 
 
+light_colors = Ain.LightChange(Ain, Bin, Cin, Din, Aout, Bout)
+print("Updated Light Colors: Ain -", light_colors[0], ", Bin -", light_colors[1], ", Cin -", light_colors[2], ", Din -", light_colors[3])
+
+time_intervals = Ain.TimeInterval(Ain, Bin, Cin, Din, Car_Total)
+print("Updated Time Intervals: Ain -", time_intervals[0], ", Bin -", time_intervals[1], ", Cin -", time_intervals[2], ", Din -", time_intervals[3])
+
+while True:
+    if Ain.Light_Color == "Red":
+        light_colors = Ain.LightChange(Ain, Bin, Cin, Din, Aout, Bout)
+        print("Updated Light Colors: Ain -", light_colors[0], ", Bin -", light_colors[1], ", Cin -", light_colors[2], ", Din -", light_colors[3])
+        time.sleep(Ain.Time_Interval)
+    else:
+        light_colors = Ain.LightChange(Ain, Bin, Cin, Din, Aout, Bout)
+        print("Updated Light Colors: Ain -", light_colors[0], ", Bin -", light_colors[1], ", Cin -", light_colors[2], ", Din -", light_colors[3])
+        time.sleep(Cin.Time_Interval)
+
+
+print(Car_Total)
+print(P_Total)
+print(vars(Ain))
+print(vars(Bin))
+print(vars(Cin))
+print(vars(Din))
+print(vars(Aout))
+print(vars(Bout))
+print(vars(Cout))
+print(vars(Dout))
--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 from SwiftRoute import CarCounter
+import csv
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
+
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
-credentials = {
-    'adil': 'adil786',
-    'arveen': 'arveen786'
-}
+
+
+roads = {}
+road_A = {}
+road_B = {}
+road_C = {}
+road_D = {}
 
 class Road:
     def __init__(self, Name: str, Light_Color: str, Direction: str, Vehicle_Amount=0, People_Amount=0, Time_Interval=180):
@@ -17,19 +24,36 @@ class Road:
         self.Vehicle_Amount = Vehicle_Amount
         self.People_Amount = People_Amount
         self.Time_Interval = Time_Interval
+    
+    def LightChange(self, Ain, Bin, Cin, Din, Aout, Bout):
+        if 3 * Bin.People_Amount + 3 * Bout.People_Amount > Bin.Vehicle_Amount + Bout.Vehicle_Amount:
+            Ain.Light_Color = "Green"
+            Bin.Light_Color = "Red"
+            Cin.Light_Color = "Red"
+            Din.Light_Color = "Green"
+        elif 3 * Bin.People_Amount + 3 * Bout.People_Amount > Ain.Vehicle_Amount + Aout.Vehicle_Amount:
+            Ain.Light_Color = "Green"
+            Bin.Light_Color = "Red"
+            Cin.Light_Color = "Red"
+            Din.Light_Color = "Green"
+        else:
+            pass
+        return Ain.Light_Color, Bin.Light_Color, Cin.Light_Color, Din.Light_Color
 
-roads = {}
+    def TimeInterval(self, Ain, Bin, Cin, Din, C_Total):
+        Ain.Time_Interval = 180 * (Ain.Vehicle_Amount + Din.Vehicle_Amount) / C_Total
+        Bin.Time_Interval = (180 * (Cin.Vehicle_Amount + Bin.Vehicle_Amount) / C_Total) + (180 - Ain.Time_Interval)
+        Cin.Time_Interval = (180 * (Cin.Vehicle_Amount + Bin.Vehicle_Amount) / C_Total) + (180 - Ain.Time_Interval)
+        Din.Time_Interval = 180 * (Ain.Vehicle_Amount + Din.Vehicle_Amount) / C_Total
+            
+        return Ain.Time_Interval, Bin.Time_Interval, Cin.Time_Interval, Din.Time_Interval
 
-def run_traffic_counting():
-    Traffic1 = CarCounter('traffic_video1.mp4')
-    Traffic2 = CarCounter('traffic_video2.mp4')
-    Traffic3 = CarCounter('traffic_video3.mp4')
-    Traffic4 = CarCounter('traffic_video4.mp4')
-
-    roads['A'] = Road("A", "Green", "East", Traffic1[0] + Traffic1[1], Traffic1[4])
-    roads['B'] = Road("B", "Red", "North", Traffic2[0] + Traffic2[1], Traffic2[4])
-    roads['C'] = Road("C", "Red", "West", Traffic3[0] + Traffic3[1], Traffic3[4])
-    roads['D'] = Road("D", "Green", "South", Traffic4[0] + Traffic4[1], Traffic4[4])
+def process_video(video):
+    try:
+        return CarCounter(video)  # Replace this with your actual CarCounter logic
+    except Exception as e:
+        print(f"Error processing {video}: {e}")
+        return [0, 0, 0, 0, 0]  # Default empty result
 
 @app.route('/')
 def index():
@@ -40,10 +64,12 @@ def login():
     username = request.form['username']
     password = request.form['password']
 
-    if username in credentials and credentials[username] == password:
-        session['username'] = username
-        run_traffic_counting() # Start traffic counting on successful login
-        return redirect(url_for('dashboard'))
+    with open('credentials.txt','r') as f:
+        for line in f.readlines(): 
+            u1,p1=line.strip().split(':')
+            if username==u1:
+                    if password == p1:
+                        return redirect(url_for('dashboard'))
 
     flash('Invalid credentials. Please try again.')
     return redirect(url_for('index'))
@@ -57,32 +83,68 @@ def dashboard():
     return render_template('dashboard1.html', username=username)
 
 @app.route('/update_traffic')
-        
 def update_traffic():
-    if not roads:
-        return jsonify({"error": "Traffic data is not available."}), 500
+    # Define video files
+    videos = ['Road_A.mp4', 'Road_B.mp4', 'Road_C.mp4', 'Road_D.mp4']
 
-    Ain = roads['A']
-    Bin = roads['B']
-    Cin = roads['C']
-    Din = roads['D']
+    # Use ProcessPoolExecutor to run in parallel
+    with ProcessPoolExecutor(max_workers=len(videos)) as executor:
+        results = list(executor.map(process_video, videos))
+
+    # Extract results
+    Traffic1, Traffic2, Traffic3, Traffic4 = results
+
+    Ain = Road("A", "Green", "East", Traffic1[0] + Traffic1[1], Traffic1[4])
+    Bin = Road("B", "Red", "North", Traffic2[0] + Traffic2[1], Traffic2[4])
+    Cin = Road("C", "Red", "West", Traffic3[0] + Traffic3[1], Traffic3[4])
+    Din = Road("D", "Green", "South", Traffic4[0] + Traffic4[1], Traffic4[4])
+
     C_Total = Ain.Vehicle_Amount + Bin.Vehicle_Amount + Cin.Vehicle_Amount + Din.Vehicle_Amount
     Ain.Time_Interval = 180 * (Ain.Vehicle_Amount + Din.Vehicle_Amount) / C_Total
     Bin.Time_Interval = (180 * (Cin.Vehicle_Amount + Bin.Vehicle_Amount) / C_Total) + (180 - Ain.Time_Interval)
     Cin.Time_Interval = (180 * (Cin.Vehicle_Amount + Bin.Vehicle_Amount) / C_Total) + (180 - Ain.Time_Interval)
     Din.Time_Interval = 180 * (Ain.Vehicle_Amount + Din.Vehicle_Amount) / C_Total
-   
-    data = {
-        'car_total': Ain.Vehicle_Amount + Bin.Vehicle_Amount + Cin.Vehicle_Amount + Din.Vehicle_Amount,
-        'pedestrian_total': Ain.People_Amount + Bin.People_Amount + Cin.People_Amount + Din.People_Amount,
-        'Ain': Ain.Vehicle_Amount,
-        'Bin': Bin.Vehicle_Amount,
-        'Cin': Cin.Vehicle_Amount,
-        'Din': Din.Vehicle_Amount,
-        'ain_time': Ain.Time_Interval,
-        'bin_time': Bin.Time_Interval,
-        'cin_time': Cin.Time_Interval,
-        'din_time': Din.Time_Interval,
+
+@app.route('/realtime_update')
+def realtime_update():
+    global road_A
+    global road_B
+    global road_C
+    global road_D
+
+    with open('Vehicle_Database_A.csv', 'r') as f:
+        reader = csv.DictReader(f)
+        road_inst = list(reader)
+        for a in road_inst:
+            road_A = a
+    with open('Vehicle_Database_B.csv', 'r') as f:
+        reader = csv.DictReader(f)
+        road_inst = list(reader)
+        for a in road_inst:
+            road_B = a
+    with open('Vehicle_Database_C.csv', 'r') as f:
+        reader = csv.DictReader(f)
+        road_inst = list(reader)
+        for a in road_inst:
+            road_C = a
+    with open('Vehicle_Database_D.csv', 'r') as f:
+        reader = csv.DictReader(f)
+        road_inst = list(reader)
+        for a in road_inst:
+            road_D = a
+
+
+        data = {
+        'car_total': int(road_A['Cars Down']) + int(road_B['Cars Down']) + int(road_C['Cars Down']) + int(road_D['Cars Down']) + int(road_A['Cars Up']) + int(road_B['Cars Up']) + int(road_C['Cars Up']) + int(road_D['Cars Up']),
+        'pedestrian_total': int(road_A['People Right']) + int(road_B['People Right']) + int(road_C['People Right']) + int(road_D['People Right']) + int(road_A['People Left']) + int(road_B['People Left']) + int(road_C['People Left']) + int(road_D['People Left']),
+        'Ain': int(road_A['Cars Down']) + int(road_A['Cars Up']),
+        'Bin': int(road_B['Cars Down']) + int(road_B['Cars Up']),
+        'Cin': int(road_C['Cars Down']) + int(road_C['Cars Up']),
+        'Din': int(road_D['Cars Down']) + int(road_D['Cars Up']),
+        'ain_time': int(road_A['Cars Down']),
+        'bin_time': int(road_B['Cars Down']),
+        'cin_time': int(road_C['Cars Down']),
+        'din_time': int(road_D['Cars Down']),
     }
     
     return jsonify(data)
